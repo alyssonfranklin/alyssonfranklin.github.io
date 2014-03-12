397d0248d1bf62819f3ba02caee8202e2dd67f42
alyssonfranklin <alysson.franklin@gmail.com>
2014-03-11 23:06:02 -0300

    Hello world w/ Rupy talk

---
+++ js/functions.js
@@ -0,0 +1,297 @@
+
+var table = [
+                //symbol, details, number, column position, line position
+                //imagem, 						titulo imagem, 			detalhes(+preco), 	coluna, linha
+				[ "img/004-0040-026_janela.jpg", 	"Caneleira Nike", 		"R$100,00",				1, 1,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/025-3076-196_janela.jpg", 	"Puma Pony", 			"R$100,00",				2, 1,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/025-5160-152_janela.jpg", 	"Tenis Puma", 			"R$100,00",				3, 1,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/037-2332-028_janela.jpg", 	"Tenis Reebok", 		"R$100,00",				4, 1,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/094-1007-014_janela.jpg", 	"Camisa BCA", 			"R$100,00",				5, 1,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/004-0040-026_janela.jpg", 	"Caneleira Nike", 		"R$100,00",				6, 1,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/025-3076-196_janela.jpg", 	"Puma Pony", 			"R$100,00",				7, 1,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/025-5160-152_janela.jpg", 	"Tenis Puma", 			"R$100,00",				8, 1,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				1, 2,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				2, 2,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				3, 2,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/132-9641-080_janela.jpg", 	"Adidas ADIracer", 		"R$100,00",				4, 2,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/149-8194-056_janela.jpg", 	"Mizuno baller", 		"R$100,00",				5, 2,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/004-0040-026_janela.jpg", 	"Caneleira Nike", 		"R$100,00",				6, 2,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/025-3076-196_janela.jpg", 	"Puma Pony", 			"R$100,00",				7, 2,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/025-5160-152_janela.jpg", 	"Tenis Puma", 			"R$100,00",				8, 2,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/178-2810-064_janela.jpg", 	"Estacao fitness", 		"R$100,00",				1, 3,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/190-6017-172_janela.jpg", 	"Pretorian handlers", 	"R$100,00",				2, 3,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/190-8045-006_janela.jpg", 	"Pretorian box", 		"R$100,00",				3, 3,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/446-8184-006_janela.jpg", 	"Lacoste jacket", 		"R$100,00",				4, 3,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/A52-0022-138_janela.jpg", 	"Timberland boots",		"R$100,00",				5, 3,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/004-0040-026_janela.jpg", 	"Caneleira Nike", 		"R$100,00",				6, 3,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/025-3076-196_janela.jpg", 	"Puma Pony", 			"R$100,00",				7, 3,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/025-5160-152_janela.jpg", 	"Tenis Puma", 			"R$100,00",				8, 3,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				1, 4,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				2, 4,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				3, 4,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/132-9641-080_janela.jpg", 	"Adidas ADIracer", 		"R$100,00",				4, 4,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/149-8194-056_janela.jpg", 	"Mizuno baller", 		"R$100,00",				5, 4,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				6, 4,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				7, 4,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				8, 4,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/178-2810-064_janela.jpg", 	"Estacao fitness", 		"R$100,00",				1, 5,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/190-6017-172_janela.jpg", 	"Pretorian handlers", 	"R$100,00",				2, 5,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/190-8045-006_janela.jpg", 	"Pretorian box", 		"R$100,00",				3, 5,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/446-8184-006_janela.jpg", 	"Lacoste jacket", 		"R$100,00",				4, 5,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/A52-0022-138_janela.jpg", 	"Timberland boots",		"R$100,00",				5, 5,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				6, 5,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				7, 5,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-9641-080_janela.jpg", 	"Adidas ADIracer", 		"R$100,00",				8, 5,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/004-0040-026_janela.jpg", 	"Caneleira Nike", 		"R$100,00",				1, 6,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/025-3076-196_janela.jpg", 	"Puma Pony", 			"R$100,00",				2, 6,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/025-5160-152_janela.jpg", 	"Tenis Puma", 			"R$100,00",				3, 6,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/037-2332-028_janela.jpg", 	"Tenis Reebok", 		"R$100,00",				4, 6,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/094-1007-014_janela.jpg", 	"Camisa BCA", 			"R$100,00",				5, 6,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				6, 6,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				7, 6,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				8, 6,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				1, 7,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				2, 7,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				3, 7,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/132-9641-080_janela.jpg", 	"Adidas ADIracer", 		"R$100,00",				4, 7,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/149-8194-056_janela.jpg", 	"Mizuno baller", 		"R$100,00",				5, 7,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				6, 7,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				7, 7,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				8, 7,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/178-2810-064_janela.jpg", 	"Estacao fitness", 		"R$100,00",				1, 8,			 "ou 5x de R$20,00",			"img/rating1.png",],
+				[ "img/190-6017-172_janela.jpg", 	"Pretorian handlers", 	"R$100,00",				2, 8,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/190-8045-006_janela.jpg", 	"Pretorian box", 		"R$100,00",				3, 8,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/446-8184-006_janela.jpg", 	"Lacoste jacket", 		"R$100,00",				4, 8,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/A52-0022-138_janela.jpg", 	"Timberland boots",		"R$100,00",				5, 8,			 "ou 5x de R$20,00",			"img/rating4.png",],
+				[ "img/123-4783-950_janela.jpg", 	"Farda Barca", 			"R$100,00",				6, 8,			 "ou 5x de R$20,00",			"img/rating3.png",],
+				[ "img/124-9459-006_janela.jpg", 	"Bike", 				"R$100,00",				7, 8,			 "ou 5x de R$20,00",			"img/rating2.png",],
+				[ "img/132-6529-078_janela.jpg", 	"Cafusa Ball", 			"R$100,00",				8, 8,			 "ou 5x de R$20,00",			"img/rating1.png"]
+			];
+
+			var camera, scene, renderer;
+			var controls;
+
+			var objects = [];
+			var targets = { sphere: [], table: [], helix: [], grid: [] };
+
+			init();
+			animate();
+
+			function init() {
+
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
+				camera.position.z = 1800;
+
+				scene = new THREE.Scene();
+
+				for ( var i = 0; i < table.length; i ++ ) {
+
+					var item = table[ i ];
+
+					var element = document.createElement( 'div' );
+					element.className = 'element';
+					element.style.backgroundColor = 'rgba(0,127,127,' + ( Math.random() * 0.5 + 0.25 ) + ')';
+
+					//var number = document.createElement( 'div' );
+					//number.className = 'number';
+					//number.textContent = i + 1;
+					//element.appendChild( number );
+
+					//adicionar o createElement para imagem e seus atributos aqui
+					//var imgBox = document.createElement( 'div' );
+					var image = document.createElement( 'img' );
+					//imgBox.className = 'imgBox';
+					image.className = 'image';
+					image.src = item[ 0 ];
+					element.appendChild( image );
+
+					var details = document.createElement( 'div' );
+					details.className = 'details';
+					details.innerHTML = '<img src="' + item[ 6 ] + '"><p class="prodTitle">' + item[ 1 ] + '</p><p class="prodPriceTo">' + item[ 2 ] + '</p><p class="prodInstallment">' + item[ 5 ] + '</p>';
+					element.appendChild( details );
+
+					var object = new THREE.CSS3DObject( element );
+					object.position.x = Math.random() * 4000 - 2000;
+					object.position.y = Math.random() * 4000 - 2000;
+					object.position.z = Math.random() * 4000 - 2000;
+					scene.add( object );
+
+					objects.push( object );
+
+				}
+
+				// table
+
+				for ( var i = 0; i < objects.length; i ++ ) {
+
+					var item = table[ i ];
+
+					var object = new THREE.Object3D();
+					object.position.x = ( item[ 3 ] * 160 ) - 1540;
+					object.position.y = - ( item[ 4 ] * 200 ) + 1100;
+
+					targets.table.push( object );
+
+				}
+
+				// sphere
+
+				var vector = new THREE.Vector3();
+
+				for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+					var phi = Math.acos( -1 + ( 2 * i ) / l );
+					var theta = Math.sqrt( l * Math.PI ) * phi;
+
+					var object = new THREE.Object3D();
+
+					object.position.x = 1000 * Math.cos( theta ) * Math.sin( phi );
+					object.position.y = 1000 * Math.sin( theta ) * Math.sin( phi );
+					object.position.z = 1000 * Math.cos( phi );
+
+					vector.copy( object.position ).multiplyScalar( 2 );
+
+					object.lookAt( vector );
+
+					targets.sphere.push( object );
+
+				}
+
+				// helix
+
+				var vector = new THREE.Vector3();
+
+				for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+					var phi = i * 0.175 + Math.PI;
+
+					var object = new THREE.Object3D();
+
+					object.position.x = 1100 * Math.sin( phi );
+					object.position.y = - ( i * 8 ) + 450;
+					object.position.z = 1100 * Math.cos( phi );
+
+					vector.copy( object.position );
+					vector.x *= 2;
+					vector.z *= 2;
+
+					object.lookAt( vector );
+
+					targets.helix.push( object );
+
+				}
+
+				// grid
+
+				for ( var i = 0; i < objects.length; i ++ ) {
+
+					var object = new THREE.Object3D();
+
+					object.position.x = ( ( i % 5 ) * 400 ) - 800;
+					object.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
+					object.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
+
+					targets.grid.push( object );
+
+				}
+
+				//
+
+				renderer = new THREE.CSS3DRenderer();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.domElement.style.position = 'absolute';
+				document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+				//
+
+				controls = new THREE.TrackballControls( camera, renderer.domElement );
+				controls.rotateSpeed = 0.5;
+				controls.addEventListener( 'change', render );
+
+				var button = document.getElementById( 'table' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.table, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'sphere' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.sphere, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'helix' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.helix, 2000 );
+
+				}, false );
+
+				var button = document.getElementById( 'grid' );
+				button.addEventListener( 'click', function ( event ) {
+
+					transform( targets.grid, 2000 );
+
+				}, false );
+
+				transform( targets.table, 5000 );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function transform( targets, duration ) {
+
+				TWEEN.removeAll();
+
+				for ( var i = 0; i < objects.length; i ++ ) {
+
+					var object = objects[ i ];
+					var target = targets[ i ];
+
+					new TWEEN.Tween( object.position )
+						.to( { x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration )
+						.easing( TWEEN.Easing.Exponential.InOut )
+						.start();
+
+					new TWEEN.Tween( object.rotation )
+						.to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+						.easing( TWEEN.Easing.Exponential.InOut )
+						.start();
+
+				}
+
+				new TWEEN.Tween( this )
+					.to( {}, duration * 2 )
+					.onUpdate( render )
+					.start();
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				TWEEN.update();
+				controls.update();
+
+			}
+
+			function render() {
+
+				renderer.render( scene, camera );
+
+			}
